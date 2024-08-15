b5b431122c3621544875a604d64ca3fd48ea6e9f
Usman Arif <usmanarif.9219@gmail.com>
2024-08-15 17:53:45 +0500

    remove web code

--- routes/web.php
+++ routes/web.php
@@ -25,17 +25,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/mtn-momo', function () {
     return view('landingpage.mtn-momo');
 });
